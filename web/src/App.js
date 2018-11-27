de8d58d52e2da5bf966e0fe4f33023685b605cd5
Aaron Dosser <adosser@ded.zone>
2018-11-27 07:20:43 -0500

    added env for CRA, added styling for posts

--- web/src/App.js
+++ web/src/App.js
@@ -9,6 +9,9 @@ class App extends Component {
         <div className="main-container">
           <textarea className="new-post" placeholder="write something"></textarea>
           <button className="submit-post">+</button>
+          <div className="post">
+            <p className="post-content">wow</p>
+          </div>
         </div>
       </Fragment>
     );
